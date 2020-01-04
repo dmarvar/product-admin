d18d23a08c2c5f5cacf3c768646b9a4c1d614a16
Arturo Martinez <d.martinez@sisproconsultores.com>
2020-01-03 21:39:59 -0500

    Updates

---
+++ precache-manifest.5a0a27df0fd1b289c30df045b6eafc61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b3e4579da2f9f61f00a7fc74154aa4",
+    "url": "/product-admin/index.html"
+  },
+  {
+    "revision": "4f982bc7d75076129121",
+    "url": "/product-admin/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "736e42e0e5ada2be9159",
+    "url": "/product-admin/static/css/main.e762e8ac.chunk.css"
+  },
+  {
+    "revision": "4f982bc7d75076129121",
+    "url": "/product-admin/static/js/2.8a441ff5.chunk.js"
+  },
+  {
+    "revision": "0836f78d5cf5ae1e31540d0bf8c80c8e",
+    "url": "/product-admin/static/js/2.8a441ff5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "736e42e0e5ada2be9159",
+    "url": "/product-admin/static/js/main.873ae487.chunk.js"
+  },
+  {
+    "revision": "390b24ca3cdbdfe65bd0",
+    "url": "/product-admin/static/js/runtime-main.69f149e0.js"
+  }
+]);
